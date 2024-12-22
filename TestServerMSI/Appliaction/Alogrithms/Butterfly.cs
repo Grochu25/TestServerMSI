ca6f56c4a3b02fcd75a256c8db0d551717041851
Aleksander Fuchs <17oju17@gmail.com>
2024-12-22 17:44:54 +0100

    Butterfly Opt. Alg. implementation

---
+++ TestServerMSI/Appliaction/Alogrithms/Butterfly.cs
@@ -0,0 +1,33 @@
+
+namespace TestServerMSI.Appliaction.Alogrithms
+{
+    public class Butterfly
+    {
+        public double[] X { get; set; }
+
+        public Butterfly(double[] X)
+        {
+            if (X == null)
+            {
+                throw new InvalidDataException();
+            }
+            this.X = X;
+        }
+
+        public double GetFragrance(fitnessFunction f, double a, double c)
+        {
+            return c * Math.Pow(f(this.X), a);
+        }
+
+        public void Clip(double[,] domain)
+        {
+            for (int i = 0; i < this.X.Length; i++)
+            {
+                if (this.X[i] > domain[1, i])
+                    this.X[i] = domain[1, i];
+                if (this.X[i] < domain[0, i])
+                    this.X[i] = domain[0, i];
+            }
+        }
+    }
+}
